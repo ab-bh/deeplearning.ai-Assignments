599eb1302bcb04438293cebc64e98767bdbac8f9
Abhishek Bhatt <abhishekanitahc@gmail.com>
2017-10-10 23:34:18 +0530

    Improved util code

--- utils.py
+++ utils.py
@@ -5,47 +5,45 @@ from PIL import Image
 import PIL
 from scipy.misc import toimage
 
+path1 ='/home/abhishek/Desktop/Scratch_NN/training'
+path2 ='/home/abhishek/Desktop/Scratch_NN/testing'
 
-path1 = '/home/abhishek/Desktop/deeplearning/training'
-path2 = '/home/abhishek/Desktop/deeplearning/testing'
+def image_to_arr(image_list, path):
+	images_list_ = []
+	for image in image_list:
+		im = Image.open(path + '/' + image)
+		im = im.resize((64,64))
+		im = np.array(im.getdata()).reshape(im.size[0], im.size[1], 3)
+		images_list_.append(im)
+	images_list_ = np.asarray(images_list_)
+	images_list_ = images_list_.astype('float32')
+	return images_list_
+
+def gen_labels(image_list, path):
+	y = []
+	for image in image_list:
+		if image[:3]=='cat': y.append(1)
+		else: y.append(0)
+	y = np.asarray(y)
+	y = y.astype('float32')
+	return y.reshape(1,y.shape[0])
+
 def load_image():
+
+	# load training/testing data
 	images_train = os.listdir(path1)
 	images_test = os.listdir(path2)
 
-	images_train_, y_train = [], []
+	# train image data and labels will be stored here
+	images_train_ = image_to_arr(images_train, path1)
 
-	# load input data
-	for image in images_train:
-		im = Image.open(path1 + '/' + image)
-		im = im.resize((64,64))
-		im = np.array(im.getdata()).reshape(im.size[0], im.size[1], 3)
-		images_train_.append(im)
-	images_train_ = np.asarray(images_train_)
-
-	# load input labels
-	for image in images_train:
-		if image[:3]=='cat': y_train.append(1)
-		else: y_train.append(0)
-	y_train = np.asarray(y_train)
-	y_train = y_train.reshape(1,y_train.shape[0])
+	## test image data will be stored here 
+	images_test_  = image_to_arr(images_test, path2)
+	
 	#toimage(images_train_[0]).show() # to see image back
 
-	## testing data
-	images_test_, y_test = [], []
-	for image in images_test:
-		im = Image.open(path2 + '/' + image)
-		im = im.resize((64,64))
-		im = np.array(im.getdata()).reshape(im.size[0], im.size[1], 3)
-		images_test_.append(im)
-	images_test_ = np.asarray(images_test_)
-
-	# load input labels
-	for image in images_test:
-		if image[:3]=='cat': y_test.append(1)
-		else: y_test.append(0)
-	y_test = np.asarray(y_test)
-	y_test = y_test.reshape(1,y_test.shape[0])		
+	# load train/test labels
+	y_train = gen_labels(images_train, path1)
+	y_test  = gen_labels(images_test, path2) 
 	
-	# classes
-	classes = np.asarray([b'not cat', b'cat'])
-	return images_train_, y_train, images_test_, y_test, classes
+	return images_train_, y_train, images_test_, y_test
